ca260d84fa3a00af50fa98a911934ed5cf9ae49a
Munish G580 <munishapc@gmail.com>
2015-04-04 23:30:28 +0530

    specifying explicitly private interface

--- lib/structure_flatter/array_hash_structure.rb
+++ lib/structure_flatter/array_hash_structure.rb
@@ -2,25 +2,27 @@ require 'active_support/core_ext/string'
 
 module StructureFlatter
   class ArrayHashStructure
+    
+    attr_reader :ahs
 
-    # initializing element
+    # Initializing element
     # passed structure should be a simple or complex structure comprising Hashes and Arrays
     def initialize(array_hash_structure = nil)
-      @array_hash_structure = array_hash_structure
+      @ahs = array_hash_structure
     end
 
-    # it flattens the given structure
-    def flatten_structure!(array_hash_structure = @array_hash_structure)
-      case array_hash_structure
+    # Flattening the given structure
+    def flatten_structure!(strt = ahs)
+      case strt
         when Array
-          array_hash_structure.each do |array_element|
+          strt.each do |array_element|
             flatten_structure!(array_element) if ((array_element.class == Array) || (array_element.class == Hash))
           end
         when Hash
-          array_hash_structure.each do |hash_key, hash_value|
+          strt.each do |hash_key, hash_value|
             if hash_value.class == Array
               # trying to flatten the value
-              ArrayHashStructure.flatten_key_value_pair!(hash_key, hash_value)
+              flatten_key_value_pair!(hash_key, hash_value)
               # recursive call
               flatten_structure!(hash_value)
             elsif hash_value.class == Hash
@@ -35,21 +37,23 @@ module StructureFlatter
         else
           # do nothing
       end
-      return array_hash_structure
+      strt
     end
 
-    # it tries to flatten the given key value pair
-    def self.flatten_key_value_pair!(key, value)
-      if self.is_key_value_pair_flattable?(key, value)
+    private
+
+    # Trying to flatten the given key value pair
+    def flatten_key_value_pair!(key, value)
+      if is_key_value_pair_flattable?(key, value)
         value.each_index do |index|
           value[index] = value[index].values.first
         end
       end
-      return [key, value]
+      [key, value]
     end
 
-    # check if given key value pair is flattable
-    def self.is_key_value_pair_flattable?(key, value)
+    # Checking if given key value pair is flattable
+    def is_key_value_pair_flattable?(key, value)
       # key should be plural
       return false unless key.to_s.pluralize == key.to_s
       # value should be Array
@@ -63,7 +67,8 @@ module StructureFlatter
         # its key should be singular form of original key
         return false unless (value_element.keys.first.to_s != key.to_s) && (value_element.keys[0].to_s.pluralize == key.to_s)
       end
-      return true
+      true
     end
   end
 end
+
