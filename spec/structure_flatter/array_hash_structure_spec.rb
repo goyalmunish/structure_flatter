4526c36fb34988e39126e61a69cb6002988c54e6
Munish G580 <munishapc@gmail.com>
2015-04-04 23:50:05 +0530

    updated tests for private methods for developer's perspective

--- spec/structure_flatter/array_hash_structure_spec.rb
+++ spec/structure_flatter/array_hash_structure_spec.rb
@@ -5,57 +5,7 @@ module StructureFlatter
 
     include VariousKeyValuePairs
     include VariousStructures
-
-    describe '.is_root_flattable?' do
-      it 'returns true for simple flattable key-value pair' do
-        expect(ArrayHashStructure.is_key_value_pair_flattable?(*key_value_pair_1)).to be true
-      end
-
-      it 'returns false for non-flattable nil value' do
-        array = [:key, nil]
-        expect(ArrayHashStructure.is_key_value_pair_flattable?(*array)).to be false
-      end
-
-      it 'returns false for non-flattable empty array as value' do
-        array = [:key, []]
-        expect(ArrayHashStructure.is_key_value_pair_flattable?(*array)).to be false
-      end
-
-      it 'returns false for non-flattable empty hash as value' do
-        array = [:key, {}]
-        expect(ArrayHashStructure.is_key_value_pair_flattable?(*array)).to be false
-      end
-
-      it 'returns false for non-flattable key-value pair' do
-        expect(ArrayHashStructure.is_key_value_pair_flattable?(*key_value_pair_1_result)).to be false
-      end
-    end
-
-    describe '.flatten_root!' do
-      it 'flattens flattable key-value pair' do
-        expect(ArrayHashStructure.flatten_key_value_pair!(*key_value_pair_1)).to eq(key_value_pair_1_result)
-      end
-
-      it 'does not affect non-flattable key-value pair' do
-        expect(ArrayHashStructure.flatten_key_value_pair!(*key_value_pair_1_result)).to eq(key_value_pair_1_result)
-      end
-
-      it 'does not affect non-flattable key-value pair with nil value' do
-        array = [:key, nil]
-        expect(ArrayHashStructure.flatten_key_value_pair!(*array)).to eq(array)
-      end
-
-      it 'does not affect non-flattable key-value pair with empty array as value' do
-        array = [:key, []]
-        expect(ArrayHashStructure.flatten_key_value_pair!(*array)).to eq(array)
-      end
-
-      it 'does not affect non-flattable key-value pair with empty hash as value' do
-        array = [:key, {}]
-        expect(ArrayHashStructure.flatten_key_value_pair!(*array)).to eq(array)
-      end
-    end
-
+    
     describe '#flatten_structure!' do
       it 'returns nil structure as it is' do
         str = ArrayHashStructure.new(nil_structure).flatten_structure!
@@ -88,6 +38,59 @@ module StructureFlatter
       it 'flattens complex nested structure' do
         str = ArrayHashStructure.new(complex_nested_structure).flatten_structure!
         expect(str).to eq(complex_nested_structure_result)
+      end
+    end
+
+    # For developer
+    describe 'PRIVATE_INTERFACE' do
+      describe '#is_key_value_pair_flattable?' do
+        it 'returns true for simple flattable key-value pair' do
+          expect(ArrayHashStructure.new.send(:is_key_value_pair_flattable?, *key_value_pair_1)).to be true
+        end
+
+        it 'returns false for non-flattable nil value' do
+          array = [:key, nil]
+          expect(ArrayHashStructure.new.send(:is_key_value_pair_flattable?, *array)).to be false
+        end
+
+        it 'returns false for non-flattable empty array as value' do
+          array = [:key, []]
+          expect(ArrayHashStructure.new.send(:is_key_value_pair_flattable?, *array)).to be false
+        end
+
+        it 'returns false for non-flattable empty hash as value' do
+          array = [:key, {}]
+          expect(ArrayHashStructure.new.send(:is_key_value_pair_flattable?, *array)).to be false
+        end
+
+        it 'returns false for non-flattable key-value pair' do
+          expect(ArrayHashStructure.new.send(:is_key_value_pair_flattable?, *key_value_pair_1_result)).to be false
+        end
+      end
+
+      describe '#flatten_key_value_pair!' do
+        it 'flattens flattable key-value pair' do
+          expect(ArrayHashStructure.new.send(:flatten_key_value_pair!, *key_value_pair_1)).to eq(key_value_pair_1_result)
+        end
+
+        it 'does not affect non-flattable key-value pair' do
+          expect(ArrayHashStructure.new.send(:flatten_key_value_pair!, *key_value_pair_1_result)).to eq(key_value_pair_1_result)
+        end
+
+        it 'does not affect non-flattable key-value pair with nil value' do
+          array = [:key, nil]
+          expect(ArrayHashStructure.new.send(:flatten_key_value_pair!, *array)).to eq(array)
+        end
+
+        it 'does not affect non-flattable key-value pair with empty array as value' do
+          array = [:key, []]
+          expect(ArrayHashStructure.new.send(:flatten_key_value_pair!, *array)).to eq(array)
+        end
+
+        it 'does not affect non-flattable key-value pair with empty hash as value' do
+          array = [:key, {}]
+          expect(ArrayHashStructure.new.send(:flatten_key_value_pair!, *array)).to eq(array)
+        end
       end
     end
 
